97f289755506c53e238d8aa9b331ad449aec0301
Kenta Kageyama <kageken.tsk.3409@gmail.com>
2017-04-18 00:24:36 +0900

    rere

--- scripts/buzzer.py
+++ scripts/buzzer.py
@@ -2,43 +2,46 @@
 #encoding: utf8
 import rospy, actionlib
 from std_msgs.msg import UInt16
-from pimouse_ros.msg import MusicAction, MusicResult, MusicFeedback
+from pimouse_ros.msg import MusicAction, MusicResult, MusicFeedback # 行を追加
 
 def write_freq(hz=0):
-  bfile = "/dev/rtbuzzer0"
-  try:
-    with open(bfile,"w") as f:
-      f.write(str(hz) + "\n")
-  except IOError:
-    rospy.logerr("can't write to " + bfile)
-
-def recv_buzzer(data):
-  write_freq(data.data)
+    bfile = "/dev/rtbuzzer0"
+    try:
+        with open(bfile,"w") as f:
+            f.write(str(hz) + "\n")
+    except IOError:
+        rospy.logerr("can't write to " + bfile)
 
 def exec_music(goal):
-  r = MusicResult()
-  fb = MusicFeedback()
+    r = MusicResult()
+    fb = MusicFeedback()
 
-  for i, j in enumerate(goal.freqs):
-    fb.remaining_steps = len(goal.freqs) - i
-    music.publish_feedback(fb)
+    for i, f in enumerate(goal.freqs):
+        fb.remaining_steps = len(goal.freqs) - i
+        music.publish_feedback(fb)
 
-    if music.is_preempt_requested():
-      write_freq(0)
-      r.finished = False
-      music.set_preempted(r)
-      return
+        if music.is_preempt_requested():
+            write_freq(0)
+            r.finished = False
+            music.set_preempted(r)
+            return
 
-    write_freq(f)
-    rospy.sleep(1.0 if i >= len(goal.durations) else goal.durations[i])
+        write_freq(f)
+        rospy.sleep(1.0 if i >= len(goal.durations) else goal.durations[i])
 
-  r.finished = True
-  music.set_succeeded(r)
+    r.finished = True
+    music.set_succeeded(r)
 
+def recv_buzzer(data):
+    write_freq(data.data)
 if __name__ == '__main__':
-  rospy.init_node('buzzer')
-  rospy.Subscriber("buzzer", UInt16, recv_buzzer)
-  music = actionlib.SimpleActionServer('music', MusicAction, exec_music, False)
-  music.start()
-  rospy.on_shutdown(write_freq)
-  rospy.spin()
+    rospy.init_node('buzzer')
+    rospy.Subscriber("buzzer", UInt16, recv_buzzer)
+    music = actionlib.SimpleActionServer('music', MusicAction, exec_music, False) # 追加
+    music.start()                                                                 # 追加
+    rospy.on_shutdown(write_freq)                                                 # 追加
+    rospy.spin()
+
+# Copyright 2016 Ryuichi Ueda
+# Released under the BSD License.
+# To make line numbers be identical with the book, this statement is written here. Don't move it to the header.
